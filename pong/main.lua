407714f0fa0296afd064e2ed1b191556bcabeb1a
ldfracao <ldfracao@protonmail.com>
2021-05-05 12:25:18 -0300

    modified both paddles

--- pong/main.lua
+++ pong/main.lua
@@ -232,23 +232,11 @@ function love.update(dt)
     --
     -- paddles can move no matter what state we're in
     --
-    -- player 1
-    if love.keyboard.isDown('w') then
-        player1.dy = -PADDLE_SPEED
-    elseif love.keyboard.isDown('s') then
-        player1.dy = PADDLE_SPEED
-    else
-        player1.dy = 0
-    end
-
-    -- player 2
-    if love.keyboard.isDown('up') then
-        player2.dy = -PADDLE_SPEED
-    elseif love.keyboard.isDown('down') then
-        player2.dy = PADDLE_SPEED
-    else
-        player2.dy = 0
-    end
+    -- player 1 always follows ball y axis
+    player1.y = ball.y
+    
+    -- player 2 always follows ball y axis
+    player1.y = ball.y
 
     -- update our ball based on its DX and DY only if we're in play state;
     -- scale the velocity by dt so movement is framerate-independent
